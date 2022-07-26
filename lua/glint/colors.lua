f2a8205b7d93e2d9cc753308a87a6e5768f4e401
Matthew Laufer <matthew.j.laufer@gmail.com>
2022-07-25 23:25:27 -0700

    adjust term colors

--- lua/glint/colors.lua
+++ lua/glint/colors.lua
@@ -1,22 +1,22 @@
 local hsluv = require('glint.hsluv').to_hex
 
 return {
-    bg1 = hsluv(0, 0, 12),
-    bg2 = hsluv(0, 0, 18),
-    bg3 = hsluv(0, 0, 28),
-    fg1 = hsluv(0, 0, 80),
-    fg2 = hsluv(0, 0, 62),
-    fg3 = hsluv(0, 0, 46),
-    red = hsluv(10, 70, 60),
-    yellow = hsluv(60, 70, 70),
-    green = hsluv(130, 40, 70),
-    cyan = hsluv(190, 50, 70),
-    blue = hsluv(240, 50, 70),
-    violet = hsluv(270, 60, 70),
-    magenta = hsluv(330, 60, 70),
-    darkGreen = hsluv(130, 40, 50),
-    diffRed = hsluv(12, 30, 20),
-    diffGreen = hsluv(130, 30, 20),
-    diffBlue = hsluv(240, 30, 20),
-    diffBlue2 = hsluv(240, 40, 30),
+    bg1 = hsluv(0, 0, 12), -- #1f1f1f
+    bg2 = hsluv(0, 0, 18), -- #2c2c2c
+    bg3 = hsluv(0, 0, 28), -- #424242
+    fg1 = hsluv(0, 0, 80), -- #c6c6c6
+    fg2 = hsluv(0, 0, 62), -- #969696
+    fg3 = hsluv(0, 0, 46), -- #6d6d6d
+    red = hsluv(12, 70, 60), -- #e76969
+    yellow = hsluv(60, 70, 70), -- #cba65d
+    green = hsluv(128, 40, 70), -- #83b883
+    cyan = hsluv(212, 40, 70), -- #89b2bc
+    blue = hsluv(248, 56, 70), -- #8eaddb
+    violet = hsluv(280, 60, 70), -- #b8a0de
+    magenta = hsluv(330, 60, 70), -- #e093c3
+    darkGreen = hsluv(128, 40, 50), -- #5a805a
+    diffRed = hsluv(12, 30, 20), -- #482727
+    diffGreen = hsluv(128, 30, 20), -- #263427
+    diffBlue = hsluv(248, 30, 20), -- #29313d
+    diffBlue2 = hsluv(248, 40, 30), -- #39475d
 }
